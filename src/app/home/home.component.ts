5630c3a0db134644b58fec72a9fb6e2f887aa12b
prasad471 <44232152+prasad471@users.noreply.github.com>
2018-11-04 22:57:44 +0530

    Add files via upload
    created master branch

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+
+  h1Style: boolean = false;
+  users: Object;
+
+  constructor(private data: DataService ) { }
+
+  ngOnInit() {
+    this.data.getUsers().subscribe(data =>{
+      this.users = data;
+      console.log(data);
+    })
+  }
+
+  firstClick()
+  {
+    //console.log('click me')
+    //this.h1Style = true;
+    
+  }
+
+}
